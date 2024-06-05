34d623004334f5139a34f76fdbaf1bf76b0cf257
RROII <roitizon@gmail.com>
2024-06-05 12:31:57 +0200

    Add files via upload

---
+++ codigo/p4/topics.hpp
@@ -0,0 +1,5 @@
+
+const char* temperatura_topic = "casa/sensores/temperatura7";
+const char* humidade_topic = "casa/sensores/humidade7";
+const char* depuracion_topic = "depuracion7";  
+const char* rele_topic = "casa/actuadores/rele7";  
